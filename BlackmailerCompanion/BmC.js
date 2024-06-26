6df90666ee05bda5c2327b85faf90c3dde4c9e98
rbnsky <robin@rofili.de>
2024-06-26 12:53:44 +0200

    bmc

--- BlackmailerCompanion/BmC.js
+++ BlackmailerCompanion/BmC.js
@@ -1,3 +1,15 @@
 "use strict";
 console.log("Start");
 let chosenCharacter;
+window.addEventListener("load", handleLoad);
+function handleLoad(_event) {
+    let mail = document.querySelector("div#mail");
+    mail.addEventListener("click", placeletter);
+    document.addEventListener("keydown", chooseletter);
+}
+function placeletter(_event) {
+    console.log(_event);
+}
+function chooseletter(_event) {
+    console.log(_event);
+}
--- BlackmailerCompanion/BmC.ts
+++ BlackmailerCompanion/BmC.ts
@@ -1,2 +1,17 @@
 console.log("Start");
 let chosenCharacter: string;
+window.addEventListener("load", handleLoad);
+
+function handleLoad(_event: Event): void {
+    let mail: HTMLElement = <HTMLElement>document.querySelector("div#mail");
+    mail.addEventListener("click", placeletter);
+    document.addEventListener("keydown", chooseletter);
+}
+
+function placeletter(_event: MouseEvent): void {
+    console.log(_event);
+}
+
+function chooseletter(_event: KeyboardEvent): void {
+    console.log(_event);
+}
