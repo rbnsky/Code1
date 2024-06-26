e458c53e68cd41f89ddc5212ca77748d33ab8eeb
rbnsky <robin@rofili.de>
2024-06-26 13:19:37 +0200

    BmC+DomTest update

--- BlackmailerCompanion/BmC.js
+++ BlackmailerCompanion/BmC.js
@@ -4,12 +4,21 @@ let chosenCharacter;
 window.addEventListener("load", handleLoad);
 function handleLoad(_event) {
     let mail = document.querySelector("div#mail");
-    mail.addEventListener("click", placeletter);
-    document.addEventListener("keydown", chooseletter);
+    mail.addEventListener("click", placeCharacter);
+    document.addEventListener("keydown", chooseCharacter);
 }
-function placeletter(_event) {
+function placeCharacter(_event) {
     console.log(_event);
+    let x = _event.offsetX;
+    let y = _event.offsetY;
+    let mail = _event.target;
+    let letter = document.createElement("span");
+    mail.appendChild(letter);
+    letter.textContent = chosenCharacter;
+    letter.style.left = x + "px";
+    letter.style.top = y + "px";
 }
-function chooseletter(_event) {
-    console.log(_event);
+function chooseCharacter(_event) {
+    // console.log(_event);
+    chosenCharacter = _event.key;
 }
--- BlackmailerCompanion/BmC.ts
+++ BlackmailerCompanion/BmC.ts
@@ -4,14 +4,25 @@ window.addEventListener("load", handleLoad);
 
 function handleLoad(_event: Event): void {
     let mail: HTMLElement = <HTMLElement>document.querySelector("div#mail");
-    mail.addEventListener("click", placeletter);
-    document.addEventListener("keydown", chooseletter);
+    mail.addEventListener("click", placeCharacter);
+    document.addEventListener("keydown", chooseCharacter);
 }
 
-function placeletter(_event: MouseEvent): void {
+function placeCharacter(_event: MouseEvent): void {
     console.log(_event);
+    let x: number = _event.offsetX;
+    let y: number = _event.offsetY;
+
+    let mail: HTMLElement = <HTMLElement>_event.target;
+    let letter: HTMLElement = document.createElement("span");
+    mail.appendChild(letter);
+
+    letter.textContent = chosenCharacter;
+    letter.style.left = x +"px";
+    letter.style.top = y +"px";
 }
 
-function chooseletter(_event: KeyboardEvent): void {
-    console.log(_event);
+function chooseCharacter(_event: KeyboardEvent): void {
+    // console.log(_event);
+    chosenCharacter =_event.key;
 }
