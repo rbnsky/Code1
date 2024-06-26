54ed809829bf2f4e6588be879f53387643f3f6f0
rbnsky <robin@rofili.de>
2024-06-26 13:28:45 +0200

    den eltern werden jetzt die kinder entogen ja man

--- BlackmailerCompanion/BmC.js
+++ BlackmailerCompanion/BmC.js
@@ -17,8 +17,14 @@ function placeCharacter(_event) {
     letter.textContent = chosenCharacter;
     letter.style.left = x + "px";
     letter.style.top = y + "px";
+    letter.addEventListener("click", deleteCharacter);
 }
 function chooseCharacter(_event) {
     // console.log(_event);
     chosenCharacter = _event.key;
+}
+function deleteCharacter(_event) {
+    let target = _event.target;
+    let parent = target.parentNode;
+    parent.removeChild(target);
 }
--- BlackmailerCompanion/BmC.ts
+++ BlackmailerCompanion/BmC.ts
@@ -20,9 +20,17 @@ function placeCharacter(_event: MouseEvent): void {
     letter.textContent = chosenCharacter;
     letter.style.left = x +"px";
     letter.style.top = y +"px";
+
+    letter.addEventListener("click", deleteCharacter);
 }
 
 function chooseCharacter(_event: KeyboardEvent): void {
     // console.log(_event);
     chosenCharacter =_event.key;
+}
+
+function deleteCharacter(_event: MouseEvent): void {
+    let target: Node = <Node>_event.target;
+    let parent: Node = <Node>target.parentNode;
+    parent.removeChild(target);
 }
